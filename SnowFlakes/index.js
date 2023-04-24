6501057ad5ce2082338dd93ffd00fb409d4d65e5
WinkJie <295213085@qq.com>
2023-04-24 17:29:03 +0800

    整理

--- SnowFlakes/index.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $(".liList").hide();
-    $(".list > li > a").hover(function () {
-        $(this).addClass("current").siblings().show().parent().siblings().children("a").removeClass("current").next().hide();
-    });
-    $("span:hidden").click(function () {
-      $(this).parent().hide();
-    });
-});
