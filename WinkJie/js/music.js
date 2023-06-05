e2d834d29a6457c2e2f2a2f2f3d23c4da61a02ad
WinkJie <295213085@qq.com>
2023-06-05 23:22:10 +0800

    12

--- WinkJie/js/music.js
+++ WinkJie/js/music.js
@@ -4,9 +4,8 @@
 感谢 @武恩赐 提供的 MetingAPI
 https://api.wuenci.com/meting/api/
 
-作者: imsyy
-主页：https://www.imsyy.top/
-GitHub：https://github.com/imsyy/home
+作者: WinkJie
+主页：https://winkjie.gitee.io/winkjie/WinkJie/
 版权所有，请勿删除
 */
 let server = "netease"; //netease: 网易云音乐; tencent: QQ音乐; kugou: 酷狗音乐; xiami: 虾米; kuwo: 酷我
